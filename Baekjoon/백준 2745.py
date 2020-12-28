5f6de59222ff0b2112fc2e52efb1c8e99e33d12d
minseojo <64322765+jms0531@users.noreply.github.com>
2020-12-29 05:28:18 +0900

    python

--- Baekjoon/백준 2745.py
+++ Baekjoon/백준 2745.py
@@ -1,5 +1,15 @@
 n, b = input().split()
 b = int(b)
+answer = 0
+arr = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+for i in range(len(n)):
+    answer += arr.find(n[len(n)-i-1])*(b**i)
+print(answer)
+
+# 다른 답안
+n, b = input().split()
+b = int(b)
 answer, cnt = 0, 0
 
 for i in n[::-1]:
